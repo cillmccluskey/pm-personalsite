20a0707ea8318594cdd9354445054e1de4448bce
Priscilla McCluskey <cillmccluskey@gmail.com>
2022-02-25 16:59:45 -0500

    yay javascript

--- personalsite.js
+++ personalsite.js
@@ -1,4 +1,6 @@
 console.log('mic check, this thing on??')
 
-
-
+let mobileAboutMe = document.querySelector('.about-me');
+if (window.innerWidth <= 768) {
+    mobileAboutMe.innerText = "I'm an aspiring Front End Web Developer with a digital marketing background. Through learning web development I can  now bring the creative ideas in my head to life and onto peoples' screens."
+  } 
