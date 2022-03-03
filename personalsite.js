1a46dbab71602d2871fd09433c3138ac7ed5c0b9
Priscilla McCluskey <cillmccluskey@gmail.com>
2022-03-03 16:48:15 -0500

    add page overlay with 'under construction' messaging

--- personalsite.js
+++ personalsite.js
@@ -4,3 +4,21 @@ let mobileAboutMe = document.querySelector('.about-me');
 if (window.innerWidth <= 768) {
     mobileAboutMe.innerText = "I'm an aspiring Front End Web Developer with a digital marketing background. Through learning web development I can  now bring the creative ideas in my head to life and onto peoples' screens."
   } 
+
+
+  window.addEventListener('load', function() {
+    setTimeout(
+      function open(e) {
+        document.querySelector('.popup').style.display = "block";
+      },
+      2500
+    )
+  })
+
+  document.querySelector('#close').addEventListener('click', function() {
+    document.querySelector('.popup').style.display = 'none';
+  })
+
+  document.querySelector('#modalBtn').addEventListener('click', function() {
+    document.querySelector('.popup').style.display = 'none';
+  })
